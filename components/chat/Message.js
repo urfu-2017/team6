eed6a8a8caafd7c887028c61a740d322db549f7d
d.fomin <d.fomin@skbkontur.ru>
2018-03-18 17:17:17 +0500

    несколько компонентов

---
+++ components/chat/Message.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Message extends React.Component {
+    state = { greeting: 'Hello world!' }
+
+    render() {
+        return (
+            <div>
+                {this.state.greeting}
+            </div>
+        )
+    }
+}
