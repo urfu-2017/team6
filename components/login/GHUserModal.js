537161936bf91dda66e91f258d82c0796bc1a2fd
d.fomin <d.fomin@skbkontur.ru>
2018-03-18 17:35:20 +0500

    заглушки компонентов

--- components/login/GHUserModal.js
+++ components/login/GHUserModal.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class LoginModal extends React.Component {
+export default class GHUserModal extends React.Component {
     state = { greeting: 'Hello world!' }
 
     render() {
