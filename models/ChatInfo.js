e261875d2564dcaa581afa561b10c6418db7b43b
Damir Shaikhislamov <jeminay.dev@gmail.com>
2018-03-25 22:24:05 +0500

    fix flow syntax

--- models/ChatInfo.js
+++ models/ChatInfo.js
@@ -7,19 +7,22 @@ interface ChatInfoType {
     id: number,
     name: string,
     members: Array<UserInfo>,
-    lastMessage: ?Message
+    lastMessage?: Message
 }
 
 export default class ChatInfo implements ChatInfoType {
     id: number
     name: string
     members: Array<UserInfo>
-    lastMessage: ?Message
+    lastMessage: Message
 
     constructor({ id, name, members = [], lastMessage }: ChatInfoType) {
         this.id = id
         this.name = name
         this.members = members
-        this.lastMessage = lastMessage
+
+        if (lastMessage instanceof Message) {
+            this.lastMessage = lastMessage
+        }
     }
 }
