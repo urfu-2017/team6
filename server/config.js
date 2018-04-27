b4928879fce2d4b4bb6ff8102faa02676628e12a
Damir Shaikhislamov <jeminay.dev@gmail.com>
2018-04-27 19:05:26 +0500

    remove hrudb =(

--- server/config.js
+++ server/config.js
@@ -4,10 +4,8 @@ dotenv.config()
 export default {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
-    API_KEY: process.env.API_KEY,
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
     AUTH_CALLBACK: process.env.AUTH_CALLBACK,
-    MONGODB_URL: process.env.MONGODB_URL,
-    HRUDB_BASE_URL: 'https://hrudb.herokuapp.com/storage/'
+    MONGODB_URL: process.env.MONGODB_URL
 }
